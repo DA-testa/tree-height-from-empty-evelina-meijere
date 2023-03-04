0693f853e3c3aade759ae9bc3c74a6ca75dd8533
evelina-meijere <124715450+evelina-meijere@users.noreply.github.com>
2023-03-04 10:32:24 +0000

    update

--- tree_height.py
+++ tree_height.py
@@ -4,26 +4,49 @@ import threading
 
 
 def compute_height(n, parents):
-    n = input()
-      n.value = parents  
-      n.left = None
-      n.right = None
-    # Write this function
-    max_height = 0
-    leftDepth = maxDepth(n.left)
-    rightDepth = maxDepth(n.right)
+     sakne = None
+     f = [[]for i in range(n)]
+     for k in range(n):
+         if parents[k] == -1:
+            sakne = k 
+         else:
+            f[parents[k]].append(i)
 
-    if leftDepth > rightDepth:
-       return leftDepth + 1
-    else:
-       return rightDepth + 1
-    # Your code 
-    if n is None
-    return max_height
 
+
+# Write this function
+    def max_height(o):
+        augstums = 1 
+        if not f[o]:
+             return augstums
+        else:
+            for child in f[o]:
+                augstums = max(augstums, max_height(child))
+            return augstums + 1
+    return max_height(sakne)
+# Your code here
 
 def main():
+    atbilde = input("F vai I?")
+    if "I" in atbilde:
+       n = int(input())
+       parents = list(map(int,input().split()))
+    elif "F" in atbilde:
+         failanos = input()
+         file = './test/' + failanos
+         if "a" not in failanos:
+             try: 
+                 with open(file) as file1:
+                    n = int(file1.readline())
+                    parents = list(map(int, file1.readline().split()))
+             except Exception as kluda:
+                 print("kluda:", str(kluda))
+                 return
+         else:
+             print("nepareizs nosaukums")
+             return
 
+    print(compute_height(n, parents))
     # implement input form keyboard and from files
     
     # let user input file name to use, don't allow file names with letter a
